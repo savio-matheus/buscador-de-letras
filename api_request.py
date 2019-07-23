175236c21668a989b795bff2c4feaadf0c9905e3
Sávio Matheus <savio933@gmail.com>
2019-07-23 14:53:26 -0300

    Add listResults()

--- api_request.py
+++ api_request.py
@@ -1,61 +1,123 @@
 import json
 import requests
 
-api_key = # Insira aqui sua api_key
+api_key = 'apikey=0f1588763cb197f6e2c0c54553293de2' # Insira aqui sua api_key
 api_url = 'https://api.vagalume.com.br/search.'
+
 
 '''
 Parâmetros:
-	termosPesquisa: palavras-chave inseridas pelo usuário.
-	buscaPor: define o filtro a ser usado na pesquisa;
+	keyWords: palavras-chave inseridas pelo usuário.
+	filterNum: define o filtro a ser usado na pesquisa;
 	os filtros possíveis estão na lista "api_search".
 
 Retorno:
 	Objeto dicionário com os dados obtidos pela API.
 '''
-def busca(termosPesquisa, buscaPor): # Lembrete: criar condições para cada código http
+def search(keyWords, filterNum):
 	api_search = ['art?', 'excerpt?', 'artmus?', 'alb?']
 	api_limit = '&limit=10'
 
-	url = api_url + api_search[buscaPor] + api_key \
-		+ '&q=' + termosPesquisa + api_limit
+	url = api_url + api_search[filterNum] + api_key \
+		+ '&q=' + keyWords + api_limit
 
 	while True:
-		retorno = requests.get(url)
-		if retorno.status_code == 200:
+		r = requests.get(url)
+		if r.status_code == 200:
 			break
 		else:
 			print('Tentando conexão...')
 
-	retornoDict = json.loads(retorno.content)
+	jsonDict = {'search' : api_search[filterNum]}
+	jsonDict = json.loads(r.content)
 
-	return retornoDict
+	return jsonDict
+
 
 '''
 Parâmetros:
-	musid: é o identificador da música a ser encontrada. É
-	obtido a partir dos dados retornados pela busca().
+	Objeto dicionário retornado pela search().
+
+Retorno:
+	Uma lista contendo os resultados da busca, um
+	dicionário para cada.
+'''
+def listSearchResults(jsonDict):
+	results = []
+	for i in jsonDict['response']['docs']:
+		results.append(i)
+
+	return results
+
+
+'''
+Parâmetros:
+	musid: é o identificador da música a ser encontrada.
 
 Retorno:
 	Objeto tipo dicionário contendo a letra e informações
-	básica sobre a faixa.
+	básicas sobre a faixa.
+
+	music = {
+		'artist' : artista,
+		'artUrl' : url do artista,
+		'musicName' : nome da música,
+		'musicUrl' : url da música,
+		'textOriginal' : letras da música original,
+		'musicUrlTransl' : url da música traduzida,
+		'musicTextTransl' : letras da música traduzida
+	}
 '''
-def retornaLetra(musid):
+def returnMusic(musid):
 	url = api_url + 'php?musid=' + musid + '&' + api_key
 
 	while True:
-		retorno = requests.get(url)
-		if retorno.status_code == 200:
+		r = requests.get(url)
+		if r.status_code == 200:
 			break
 		else:
 			print('Tentando conexão...')
 
-	retornoDict = json.loads(retorno.content)
+	jsonDict = json.loads(r.content)
 
-	return retornoDict
+	music = {}
+
+	music.update( {'artist' : jsonDict['art']['name']} )
+	music.update( {'artUrl' : jsonDict['art']['url']} )
+	music.update( {'musicName' : jsonDict['mus'][0]['name']} )
+	music.update( {'musicUrl' : jsonDict['mus'][0]['url']} )
+	music.update( {'textOriginal' : jsonDict['mus'][0]['text'] } )
+
+	try:
+		music.update( {'musicUrlTransl' :
+			jsonDict['mus'][0]['translate'][0]['url']} )
+		music.update( {'musicTextTransl' :
+			jsonDict['mus'][0]['translate'][0]['text']} )
+	except KeyError:
+		print('Não há tradução disponível')
+
+	return music
+
 
 if __name__ == '__main__':
 	print('Testando...')
-	print(busca('American Boy', 2))
-	print(retornaLetra('l3ade68b8gbe175fa3'))
-# https://api.vagalume.com.br/search.php?musid=3ade68b6g4946fda3&apikey={key}
+
+	keyWords = input()
+	filterWord = int(input())
+
+	busca = search(keyWords, filterWord)
+	resultados = listSearchResults(busca)
+	lyric = returnMusic(resultados[0]['id'])
+
+	print(lyric['textOriginal'])
+
+	try:
+		print('\n\n\n\n\n' + lyric['musicTextTransl'])
+	except KeyError:
+		print('\n\n\nNão há tradução disponível')
+
+'''
+Lembretes:
+	Criar condições para cada código http;
+	Receber outros dados, como discografia e imagens;
+'''
